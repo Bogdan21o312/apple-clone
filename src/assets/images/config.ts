f7803cfd5cd27ede142035935fba43a6fd7bd19b
Bogdan21o312 <lutskiv2010@gmail.com>
2022-10-22 19:54:21 +0300

    Updated images + import svg

---
+++ src/assets/images/config.ts
@@ -0,0 +1,9 @@
+import {ReactComponent as Logo} from "./logo.svg";
+import {ReactComponent as Search} from "./icons/search.svg";
+import {ReactComponent as Suitcase} from "./icons/suitcase.svg";
+
+export {
+    Logo,
+    Search,
+    Suitcase,
+}

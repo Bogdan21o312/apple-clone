9fe17e911b70b39b1bfb4168cf00d73c2c705723
Bogdan21o312 <lutskiv2010@gmail.com>
2022-10-24 18:15:31 +0300

    Created Route for page

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -2,12 +2,43 @@ import React from 'react';
 import {Navigate, Route, Routes} from "react-router-dom";
 import Home from "../pages/Home";
 import Error from "../pages/Error";
-import {ERROR_URL, HOME_URL} from "./config";
+import {
+    ACCESSORIES_URL,
+    AIR_PODS_URL,
+    ERROR_URL,
+    HOME_URL,
+    IPAD_URL,
+    IPHONE_URL,
+    MAC_URL, ONLY_ON_APPLE_URL,
+    STORE_URL, SUPPORT_URL,
+    TV_HOME_URL,
+    WATCH_URL
+} from "./config";
+import Store from "../pages/Store";
+import Mac from "../pages/Mac";
+import Ipad from "../pages/Ipad";
+import Iphone from "../pages/Iphone";
+import Watch from "../pages/Watch";
+import AirPods from "../pages/AirPods";
+import TVHome from "../pages/TV&Home";
+import OnlyOnApple from "../pages/OnlyOnApple";
+import Accessories from "../pages/Accessories";
+import Support from "../pages/Support";
 
 const AppRouter = () => {
     return (
         <Routes>
             <Route path={HOME_URL} element={<Home/>}/>
+            <Route path={STORE_URL} element={<Store/>}/>
+            <Route path={MAC_URL} element={<Mac/>}/>
+            <Route path={IPAD_URL} element={<Ipad/>}/>
+            <Route path={IPHONE_URL} element={<Iphone/>}/>
+            <Route path={WATCH_URL} element={<Watch/>}/>
+            <Route path={AIR_PODS_URL} element={<AirPods/>}/>
+            <Route path={TV_HOME_URL} element={<TVHome/>}/>
+            <Route path={ONLY_ON_APPLE_URL} element={<OnlyOnApple/>}/>
+            <Route path={ACCESSORIES_URL} element={<Accessories/>}/>
+            <Route path={SUPPORT_URL} element={<Support/>}/>
             <Route path={ERROR_URL} element={<Error/>}/>
             <Route
                 path="*"
--- src/router/config.ts
+++ src/router/config.ts
@@ -1,3 +1,16 @@
+// MainPage ====================================================================================================
 export const HOME_URL = "/home"
-
 export const ERROR_URL = "/error"
+
+// NavbarPage ====================================================================================================
+
+export const STORE_URL = "/store"
+export const MAC_URL = "/mac"
+export const IPAD_URL = "/ipad"
+export const IPHONE_URL = "/iphone"
+export const WATCH_URL = "/watch"
+export const AIR_PODS_URL = "/airpods"
+export const TV_HOME_URL = "/tv&home"
+export const ONLY_ON_APPLE_URL = "/onlyonapple"
+export const ACCESSORIES_URL = "/accessories"
+export const SUPPORT_URL = "/support"

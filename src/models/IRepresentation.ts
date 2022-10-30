adaa614d2acc2beaff71ab970ccd3a932ccb20b2
Bogdan21o312 <lutskiv2010@gmail.com>
2022-10-30 08:59:39 +0200

    Created IRepresentation.ts

---
+++ src/models/IRepresentation.ts
@@ -0,0 +1,13 @@
+export interface IRepresentation {
+    logo?: boolean
+    mainColor: string
+    imgUrl: string
+    textBig: string
+    textSubTitle?: string
+    colorSubTitle?: string
+    textMedium: string
+    firstLink: string
+    firstLinkTo: string
+    lastLink: string
+    lastLinkTo: string
+}

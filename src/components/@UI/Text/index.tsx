bd6c8106650f988be84372cc34ef550ba75b9a16
Bogdan21o312 <lutskiv2010@gmail.com>
2022-11-04 16:08:54 +0200

    Updated

--- src/components/@UI/Text/index.tsx
+++ src/components/@UI/Text/index.tsx
@@ -5,25 +5,27 @@ import {Logo} from "../../../assets/images/config";
 
 interface TextProps {
     children: ReactNode,
-    to?: string | any,
+    to?: string,
     logo?: boolean,
     color?: string,
     size?: string
+    mainColor?: string
 }
 
-export const Text: FC<TextProps> = ({children, to, color, logo, size}) => {
+export const Text: FC<TextProps> = ({children, to, color, logo, size, mainColor}) => {
+    const MainColor = `${mainColor === 'white' ? classes.White : ''}`
     const Color = `${color === 'red' ? classes.Red : ''}${color === 'orange' ? classes.Orange : ''}${color === 'blue' ? classes.Blue : ''}`
     const LogoApple = `${logo === true ? classes.Logo : classes.Remove}`
     const Size = `${size === 'big' ? classes.Big : ''}${size === 'medium' ? classes.Medium : ''}${size === 'small' ? classes.Small : ''}`
     return (
         to
             ?
-            <Link to={to} className={`${classes.Base} ${Size} ${classes.Link}`}>
+            <Link to={to} className={`${classes.Base} ${MainColor} ${Size} ${classes.Link}`}>
                 <Logo className={`${LogoApple}`}/>
                 <span className={`${Color}`}>{children}</span>
             </Link>
             :
-            <div className={`${classes.Base} ${Size}`}>
+            <div className={`${classes.Base} ${MainColor} ${Size}`}>
                 <Logo className={`${LogoApple}`}/>
                 <span className={`${Color}`}>{children}</span>
             </div>

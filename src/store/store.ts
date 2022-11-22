3c5197e10fc23748a7e3cddbd688e0c07d10ee23
Bogdan21o312 <lutskiv2010@gmail.com>
2022-11-22 14:01:00 +0200

    Updated

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,11 +1,9 @@
 import {combineReducers, configureStore} from "@reduxjs/toolkit";
-import {testAPI} from "../sevices/test";
 import {navbarAPI} from "../sevices/NavbarService";
 import {footerAPI} from "../sevices/FooterService";
 import {homeAPI} from "../sevices/HomeService";
 
 const rootReducer = combineReducers({
-    [testAPI.reducerPath]: testAPI.reducer,
     [navbarAPI.reducerPath]: navbarAPI.reducer,
     [footerAPI.reducerPath]: footerAPI.reducer,
     [homeAPI.reducerPath]: homeAPI.reducer,
